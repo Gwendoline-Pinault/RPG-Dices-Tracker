49095ef1dbbadbeef9e6b5a0b9c5fcae190018fe
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2024-09-22 19:57:02 +0200

    mise à jour du front avec la nouvelle disposition json

--- program/app.js
+++ program/app.js
@@ -1,29 +1,29 @@
 import { createPJGlobalStat, createGameStatInfos, createJDRPageContent } from './functions.js';
 
-// Récupération des données stockées dans le JSON
-const data = await fetch('/program/data.json')
-  .then(response => response.json())
-  .catch((e) => { });
-
 // Détermination de la page du JDR pour attribuer les valeurs aux variables
 const ariaPage = document.getElementById('aria-page');
-const hogwardsPage = document.getElementById('hogwards-page');
+const poudlardPage = document.getElementById('poudlard-page');
 const pathfinderPage = document.getElementById('pathfinder-page');
 let jdrName = "";
 let jdr = "";
 
 // "null" quand la page ne correspond pas à celle courante 
 if (ariaPage != null) {
-  jdr = "aria";
-  jdrName = "Aria";
+    jdr = "aria";
+    jdrName = "Aria";
 }
-else if (hogwardsPage != null) {
-  jdr = "hogwards";
-  jdrName = "Poudlard";
+else if (poudlardPage != null) {
+    jdr = "poudlard";
+    jdrName = "Poudlard";
 }
 else if (pathfinderPage != null) {
-  jdr = "pathfinder";
-  jdrName = "Pathfinder";
+    jdr = "pathfinder";
+    jdrName = "Pathfinder";
 }
+
+// Récupération des données stockées dans le JSON
+const data = await fetch(`/program/data/${jdr}.json`)
+    .then(response => response.json())
+    .catch((e) => { });
 
 createJDRPageContent(data, jdr, jdrName);
