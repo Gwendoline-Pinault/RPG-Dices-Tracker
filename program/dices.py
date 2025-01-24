14a854dd73e4e2a3c1617ad2db1900a739721eac
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2025-01-24 22:58:52 +0100

    partie Aria 24.01.2025

--- program/dices.py
+++ program/dices.py
@@ -1,7 +1,7 @@
 # Aria
-Melinda = [59, 8, 45, 18, 71, 16, 29, 30, 87, 4, ]
-Efard = [39, 74, 32, 18, 77, 3, 11, 52, 40, 14, 94, 96, ]
-Artyom = [89, ]
+Melinda = []
+Efard = []
+Artyom = []
 
 # Pathfinder
 Callum = []
