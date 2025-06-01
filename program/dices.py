0a1a46e595f154a941ab03733f50001a4e7fb7d6
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2025-06-01 23:06:42 +0200

    feat(golden sun): partie du 1er juin

--- program/dices.py
+++ program/dices.py
@@ -1,7 +1,7 @@
 # Aria
 Melinda = []
 Efard = []
-Artyom = []
+Artyom = [80, ]
 
 # Pathfinder
 Callum = []
@@ -15,6 +15,6 @@ Michael = []
 Eva = []
 
 # Golden Sun
-Takeshi = []
-Tepes = []
-Rawiri = []
+Takeshi = [14, 5, 18, 15, 8, 8, 18, 19, 5, 18, 2, 3, 3, 8, 2, ]
+Tepes = [14, 3, 7, 8, 13, 18, 20, 12, 4, 2, 3, 4, 19, 8, 17, 8, 7, ]
+Rawiri = [10, 14, 11, 11, 18, 9, 3, 14, 3, 15, 1, 4, 1, 8, 4, 20, 11, 16, 3, 13, 12, 17, 19, 5, 14, ]
