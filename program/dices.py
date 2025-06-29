c05dd82eded4c567d4444e37304b7fd05915d64c
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2025-06-29 22:00:06 +0200

    partie Golden Sun 25.06

--- program/dices.py
+++ program/dices.py
@@ -1,7 +1,7 @@
 # Aria
-Melinda = [17, 74, ]
-Efard = [90, 82, 32, 61, 36, ]
-Artyom = [92, 16, ]
+Melinda = []
+Efard = []
+Artyom = []
 
 # Pathfinder
 Callum = []
@@ -15,6 +15,6 @@ Michael = []
 Eva = []
 
 # Golden Sun
-Takeshi = []
-Tepes = []
-Rawiri = []
+Takeshi = [5,12,16,4,13,8,7,2,12,20,11,2,18,9,19,3,13,14,15,]
+Tepes = [19,11,16,14,12,8,13,5,11,5,5,1,10,7,]
+Rawiri = [13,10,19,6,1,15,17,6,3,15,15,9,13,2,12,17,7,9,18,]
