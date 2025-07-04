293a932663fa910b0055ba5abfbf61b9ebf9a0b6
Gwendoline Pinault <129307228+Gwendoline-Pinault@users.noreply.github.com>
2025-07-04 22:57:10 +0200

    Update dices.py partie Aria 4/07

--- program/dices.py
+++ program/dices.py
@@ -1,6 +1,6 @@
 # Aria
-Melinda = []
-Efard = []
+Melinda = [10,]
+Efard = [1, ]
 Artyom = []
 
 # Pathfinder
@@ -15,6 +15,6 @@ Michael = []
 Eva = []
 
 # Golden Sun
-Takeshi = [5,12,16,4,13,8,7,2,12,20,11,2,18,9,19,3,13,14,15,]
-Tepes = [19,11,16,14,12,8,13,5,11,5,5,1,10,7,]
-Rawiri = [13,10,19,6,1,15,17,6,3,15,15,9,13,2,12,17,7,9,18,]
+Takeshi = []
+Tepes = []
+Rawiri = []
