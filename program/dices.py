4b2d7105320c01929e0f0b22092815d4fc7172da
Gwendoline-Pinault <pinaultgwendoline@gmail.com>
2025-07-06 22:23:32 +0200

    add Golden Sun game

--- program/dices.py
+++ program/dices.py
@@ -1,6 +1,6 @@
 # Aria
-Melinda = [10,]
-Efard = [1, ]
+Melinda = []
+Efard = []
 Artyom = []
 
 # Pathfinder
@@ -15,6 +15,6 @@ Michael = []
 Eva = []
 
 # Golden Sun
-Takeshi = [3,6,11,4,15,15,11,9,4,16,20,15,12]
-Tepes = [7,17,14,4,9,3,16,5,18,13,20,5,1,17,10,2,16,13,2,]
-Rawiri = [11,10,17,16,4,12,7,8,13,15,10,8,19,19,1,1,3,11,]
+Takeshi = []
+Tepes = []
+Rawiri = []
